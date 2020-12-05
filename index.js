291a2b703fa7de84c9ab178bd982ba26a30ff3f0
Seth Todd <seth.todd@outlook.com>
2020-12-05 16:13:50 -0600

    commit tree 7fa226b86cdd8c07d5fc914cd915489c780aee6d

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 4;
+const num2 = 15.5;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.random() + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 8, 3, 20);
